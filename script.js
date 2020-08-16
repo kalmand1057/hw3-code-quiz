f9645cc6ef89db1d35e10cce6813aa1a1208742c
Kalena Almand <kalmand1057@gmail.com>
2020-08-15 19:49:38 -0500

    added questions

--- script.js
+++ script.js
@@ -16,6 +16,35 @@ function selectAnswer() {
 
 }
 
+//QUESTIONS
+var questions = [
+  {
+    question: "What is Javascript?",
+    options: ["type of coffee", "programming language", "type of font"],
+    answerIdx: 1
+  },
+  {
+    question: "What section of the HTML page do you place the javascript?",
+    options: ["body", "head", "footer"],
+    answerIdx: 0
+  },
+  {
+    question: "How do you add a comment in Javascript?",
+    options: ["!comment", "<!--comment-->", "//comment"],
+    answerIdx: 2
+  },
+  {
+    question: "What HTML element do you use for Javascript?",
+    options: ["<javascript>", "<script>", "<js>"],
+    answerIdx: 1
+  },
+  {
+    question: "What case do you use in Javascript?",
+    options: ["Camel Case", "Snake Case", "Pascal Case"],
+    answerIdx: 0
+  },
+]
+
 //TIMER
 
 var secondsLeft = 120;
